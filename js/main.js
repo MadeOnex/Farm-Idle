db73b76fac68d88d6b44572e50bc89654bfedcd0
Dawid <madeone162@gmail.com>
2025-10-06 14:56:08 +0200

    html skelett und css struktur

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Hi");
+const buttons = document.querySelectorAll(".toolbar-farm button");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    for (let j = 0; j < buttons.length; j++) {
+      buttons[j].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+
